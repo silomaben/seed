41fe6d602b8467d794afe32b4d1bd46f242acf1c
BenardSilomaMasikond <BenardSilomaMasikond@JITU-PC110VYA>
2024-04-01 14:18:02 +0300

    finiti

--- cypress-tests/Entrypoint.sh
+++ cypress-tests/Entrypoint.sh
@@ -8,6 +8,10 @@ npm run cy:run
 echo "** Copying test reports..."
 # Copy files after tests run
 cp -r /app/cypress /shared
+ls -la /app
+ls -la /app/cypress
+ls -la /app/cypress/reports
+ls -la /app/cypress/reports/html
 
 echo "** Copying complete."
 
