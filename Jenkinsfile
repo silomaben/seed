a803f41738815991634eb4e92a512d4a7e5aaa34
BenardSilomaMasikond <BenardSilomaMasikond@JITU-PC110VYA>
2024-04-23 16:19:48 +0300

    no api

--- Jenkinsfile
+++ Jenkinsfile
@@ -379,6 +379,9 @@ def waitForReport(podName) {
                  def podStatus = sh(script: "kubectl get pod $podName -n cypress -o jsonpath='{.status.phase}'", returnStdout: true).trim()
                     echo "Pod Status: $podStatus"
 
+                    // sh "kubectl describe pod/$cypressPod -n cypress"
+                    sh "kubectl logs -n cypress $cypressPod -c e2e-test-app"
+
                     // if (podStatus == 'Succeeded') {
                     //     sh"kubectl logs -n cypress $cypressPod -c e2e-test-app"
                     // } else if (podStatus == 'Failed') {
--- cypress/cypress/e2e/Dashboard.cy.ts
+++ cypress/cypress/e2e/Dashboard.cy.ts
@@ -2,11 +2,38 @@ import '../support/commands';
 
 it('confirm dashboard content area contains all the categories and redirects to respective pages  onclick.', () => {
     // cy.login(`${Cypress.env('QA_EMAIL')}`, `${Cypress.env('QA_PASSWORD')}`);
-    cy.visit("/auth")
+    cy.visit("/testwithsteve")
 
-    cy.wait(25000);
+    cy.get('.ml-4 > .text-900').click()
+
+    cy.wait(30000);
 
     cy.contains('body', ' Please contact your administrator').should('exist');
+
+    let numberOfItems;
+
+    // cy.addTestContext('Below are the available categories');
+
+    // cy.dataCy('content-container')
+    //     .find('[data-cy="category-item"]')
+    //     .then(($items) => {
+    //         numberOfItems = $items.length;
+
+    //         for (let i = 0; i < numberOfItems; i++) {
+    //             cy.clickCategory(i);
+    //         }
+    //     });
+});
+
+
+it('tests with steve', () => {
+    // cy.login(`${Cypress.env('QA_EMAIL')}`, `${Cypress.env('QA_PASSWORD')}`);
+    cy.visit("/auth")
+    cy.visit("/testwithsteve")
+
+    cy.wait(30000);
+
+    // cy.contains('body', ' Please contact your administrator').should('exist');
 
     let numberOfItems;
 
