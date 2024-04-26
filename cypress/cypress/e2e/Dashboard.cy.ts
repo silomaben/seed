7a174f437a7cde7a81b944a9837f9e7a04925fc6
BenardSilomaMasikond <BenardSilomaMasikond@JITU-PC110VYA>
2024-04-26 16:24:00 +0300

    baseurl localhost

--- cypress/cypress/e2e/Dashboard.cy.ts
+++ cypress/cypress/e2e/Dashboard.cy.ts
@@ -1,58 +1,67 @@
 import '../support/commands';
 
-it('confirm dashboard content area contains all the categories and redirects to respective pages  onclick.', () => {
-    // cy.login(`${Cypress.env('QA_EMAIL')}`, `${Cypress.env('QA_PASSWORD')}`);
+it('visit auth page', () => {
     cy.visit("/auth")
 
+    cy.get('button[class="p-ripple p-element p-button p-component"]').click();
 
     cy.url().then((url) => {
         cy.log('Current URL:', url);
     });
     
-    // cy.visit("/")
 
-    // cy.get('.ml-4 > .text-900').click()
+    cy.wait(2000);
 
-    cy.wait(5000);
-
-    cy.contains('body', ' Please contact your administrator').should('exist');
-
-    let numberOfItems;
-
-    // cy.addTestContext('Below are the available categories');
-
-    // cy.dataCy('content-container')
-    //     .find('[data-cy="category-item"]')
-    //     .then(($items) => {
-    //         numberOfItems = $items.length;
-
-    //         for (let i = 0; i < numberOfItems; i++) {
-    //             cy.clickCategory(i);
-    //         }
-    //     });
 });
 
 
-it('tests with steve', () => {
-    // cy.login(`${Cypress.env('QA_EMAIL')}`, `${Cypress.env('QA_PASSWORD')}`);
-    cy.visit("/auth")
+it('visit /', () => {
+    cy.visit("/")
+
+    cy.get('button[class="p-ripple p-element p-button p-component"]').click();
+
+    cy.url().then((url) => {
+        cy.log('Current URL:', url);
+    });
+    
+
+    cy.wait(2000);
+});
+
+it('visit nonexistent page', () => {
     cy.visit("/testwithsteve")
 
-    cy.wait(5000);
+    cy.get('button[class="p-ripple p-element p-button p-component"]').click();
 
-    // cy.contains('body', ' Please contact your administrator').should('exist');
+    cy.url().then((url) => {
+        cy.log('Current URL:', url);
+    });
+    
 
-    let numberOfItems;
-
-    // cy.addTestContext('Below are the available categories');
-
-    // cy.dataCy('content-container')
-    //     .find('[data-cy="category-item"]')
-    //     .then(($items) => {
-    //         numberOfItems = $items.length;
-
-    //         for (let i = 0; i < numberOfItems; i++) {
-    //             cy.clickCategory(i);
-    //         }
-    //     });
+    cy.wait(2000);
 });
+
+it('visit auth page of deployed ui', () => {
+
+    let email = "benard.masikonde@cerebriai.com"
+
+    let password = "Password123"
+
+    const args = { email, password };
+    cy.origin(
+        'https://atq.cerebri.systems/atq2/auth',
+        { args },
+        ({ email, password }) => {
+            cy.wait(2000)
+            // cy.get('#username').type(email);
+
+            // Cypress.config('pageLoadTimeout', 6000);
+            // cy.get('input[name="password"]')
+            //     .type(password)
+
+            //     .get('button[type="submit"][name="action"][value="default"]')
+            //     .click();
+        }
+    );
+});
+
